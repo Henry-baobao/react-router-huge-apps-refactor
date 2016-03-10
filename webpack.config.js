54ab56bfc8e65930639fff764f30e296c7ac8185
echenley <evan.henley@gmail.com>
2016-03-10 16:50:58 -0600

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+/*eslint-disable no-var */
+var fs = require('fs')
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+
+  devtool: 'inline-source-map',
+
+  entry: './src/app.js',
+
+  output: {
+    path: __dirname + '/__build__',
+    filename: 'app.js',
+    chunkFilename: '[id].chunk.js',
+    publicPath: '/__build__/'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
+
+}
